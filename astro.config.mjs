ede46fd95187c06b584d55c2ec39b3e8423ed758
billkapes <kapes15@yahoo.com>
2025-03-26 13:52:08 -0500

    Added preact, first island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dazzling-kringle-ce6b70.netlify.app/"
+  site: "https://dazzling-kringle-ce6b70.netlify.app/",
+  integrations: [preact()]
 });
